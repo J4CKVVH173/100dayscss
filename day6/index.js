f96cd66d625af9763f0f5ce686c43ddc5067cc10
Andrey <i78901234567890@gmail.com>
2020-02-09 17:03:06 +0300

    update. js анимация для появления и скрытия input для search. update. анимация смещения карточки для открытия до меню (доп меню пока не сделано)

--- day6/index.js
+++ day6/index.js
@@ -1,4 +1,10 @@
+
 function search() {
   const e = document.getElementById('search-input');
   e.classList.toggle('hide');
+}
+
+function extraPanel() {
+  const e = document.getElementById('panel');
+  e.classList.toggle('move-right');
 }
